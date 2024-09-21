a0c43a9cc831ec12da5deff2d9ab767c3ed50710
Gregory Farias <gregory@drcuco.com.br>
2024-09-21 18:24:11 -0300

    added seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $seeders = [
+            ComorbidadeSeeder::class
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call($seeders);
     }
 }
