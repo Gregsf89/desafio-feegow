bf8d532efe47ac28fa7a995fc6c5cec568b19c24
Gregory Farias <gregory@drcuco.com.br>
2024-09-26 16:40:58 -0300

    final touches

--- app/Jobs/MassPopulateDatabaseJob.php
+++ app/Jobs/MassPopulateDatabaseJob.php
@@ -7,7 +7,6 @@ use App\Models\Funcionario;
 use App\Models\LoteVacina;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Str;
 
 class MassPopulateDatabaseJob implements ShouldQueue
 {
