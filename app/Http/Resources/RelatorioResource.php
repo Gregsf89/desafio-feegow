737de239131f20beec6fd2d36fc068e702ef1ed6
Gregory Farias <gregory@drcuco.com.br>
2024-09-27 11:45:41 -0300

    mask

--- app/Http/Resources/RelatorioResource.php
+++ app/Http/Resources/RelatorioResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class RelatorioResource extends JsonResource
 {
@@ -27,7 +28,7 @@ class RelatorioResource extends JsonResource
         $body = [];
         $resource->each(function ($item) use (&$body) {
             $body[] = [
-                'cpf' => $item->cpf,
+                'cpf' => Str::mask($item->cpf, '*', 3),
                 'nome' => $item->nome,
                 'data_nascimento' => $item->data_nascimento->format('Y-m-d'),
                 'comorbidade' => (bool) $item->comorbidade,
